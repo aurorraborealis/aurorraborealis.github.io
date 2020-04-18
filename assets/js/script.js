3ad7d63f098e4d878e0fefbd55facc2fe516a7b3
Asusagawa Aurrora <49149529+AurorraY@users.noreply.github.com>
2020-04-18 17:33:16 +0700

    "FA Modified"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
+// ALL SOURCODE IN GUTHUB AurorraY
+function toggle(){
+  let curtain = document.getElementById('curtain');
+  curtain.classList.toggle('active')
+}
+
 $(document).ready(function () {
+  
   function clock() {
-    var now = new Date();
-    var sec = ('0' + now.getSeconds()).slice(-2);
-    var mins = ('0' + now.getMinutes()).slice(-2);
-    var hour = now.getHours();
-    var year = now.getFullYear();
-    var month = now.getUTCMonth();
-    var day = now.getDate();
-    var time = hour + ":" + mins + ":" + sec;
-    var hijri = new Intl.DateTimeFormat('en-TN-u-ca-islamic', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }).format(Date.now());
-    var masehi = new Intl.DateTimeFormat('en', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }).format(Date.now());
+    let now = new Date();
+    let sec = ('0' + now.getSeconds()).slice(-2);
+    let mins = ('0' + now.getMinutes()).slice(-2);
+    let hour = now.getHours();
+    let year = now.getFullYear();
+    let month = now.getUTCMonth();
+    let day = now.getDate();
+    let time = hour + ":" + mins + ":" + sec;
+    let hijri = new Intl.DateTimeFormat('en-TN-u-ca-islamic', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }).format(Date.now());
+    let masehi = new Intl.DateTimeFormat('en', { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' }).format(Date.now());
     
     document.getElementById("watch-clock").innerHTML = time;
     document.getElementById("date-hijri").innerHTML = hijri;
@@ -17,4 +24,20 @@ $(document).ready(function () {
     requestAnimationFrame(clock);
   }
   requestAnimationFrame(clock);
+  
+  jQuery(function ($) {
+    $.getJSON('https://muslimsalat.com/bandung/daily.json?key=b3cf4d0f3a61aef91eb32415b6ceefc9&jsoncallback=?', function (times) {
+      $('.prayerTimes')
+        .append('Today in ' + times.title)
+        .append(' Fajr: ' + times.items[0].fajr)
+        .append(' Dhuhr: ' + times.items[0].dhuhr)
+        .append(' Asr: ' + times.items[0].asr)
+        .append(' Maghrib: ' + times.items[0].maghrib)
+        .append(' Isha: ' + times.items[0].isha)
+        .append(' by MuslimSalat.com');
+    });
+  });
+
+
+
 });
